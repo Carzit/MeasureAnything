68eccb5d996c7ddfddf0ade7699bd51bd6f5f6f7
Carzit <126076978+Carzit@users.noreply.github.com>
2023-06-04 03:12:56 +0800

    Add files via upload

--- GD/GreyDetector.py
+++ GD/GreyDetector.py
@@ -1,22 +1,17 @@
 import cv2
 
-img = cv2.imread('test-MiDas.png')
-#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+img_show = cv2.imread('r1.png') # the raw image
+img_read = cv2.imread('d1.png') # the depth image
 
 def mouse_click(event, x, y, flags, para):
-    if event == cv2.EVENT_LBUTTONDOWN:  # 左边鼠标点击
-        print('PIX:', x, y)
-        print("BGR:", img[y, x])
-        #print("GRAY:", gray[y, x])
-        #print("HSV:", hsv[y, x])
-
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x,',' ,y ,',', img_read[y, x][0])
 
 if __name__ == '__main__':
     cv2.namedWindow('img', cv2.WINDOW_NORMAL)
     cv2.setMouseCallback("img", mouse_click)
     while True:
-        cv2.imshow('img', img)
-        if cv2.waitKey() == ord('q'):
+        cv2.imshow('img', img_show)
+        if cv2.waitKey() == ord('q'): # press 'q' to quit
             break
     cv2.destroyAllWindows()
